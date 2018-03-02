68470d7e05b60647f60286fe6d1816a2c3d7d590
maeybilisim <36985268+maeybilisim@users.noreply.github.com>
2018-03-02 12:04:57 +0300

    Add files via upload

---
+++ ogrenciler.js
@@ -0,0 +1,44 @@
+{"ogrenciler":[
+
+   {
+        "ogrenciNo":"1",
+		"ogrenciAdi":"Adı",
+		"ogrenciSoyad":"Soyadı",
+		"bolumAdi":"Bilgi İşlem Mühendisliği",
+		"fabrikaAdi":"Elginkan Valfsel",
+		"ogrenciSinif":"3"
+		},
+		{
+		"ogrenciNo":"2",
+		"ogrenciAdi":"Adı",
+		"ogrenciSoyad":"Soyadı",
+		"bolumAdi":"Bilgi İşlem Mühendisliği",
+		"fabrikaAdi":"Elginkan Valfsel",
+		"ogrenciSinif":"4"
+		},
+		{
+		"ogrenciNo":"3",
+		"ogrenciAdi":"Adı",
+		"ogrenciSoyad":"Soyadı",
+		"bolumAdi":"Bilgi İşlem Mühendisliği",
+		"fabrikaAdi":"Elginkan Valfsel",
+		"ogrenciSinif":"2"
+		},
+		{
+		"ogrenciNo":"1",
+		"ogrenciAdi":"Adı",
+		"ogrenciSoyad":"Soyadı",
+		"bolumAdi":"Bilgi İşlem Mühendisliği",
+		"fabrikaAdi":"Elginkan Valfsel",
+		"ogrenciSinif":"2"
+		},
+		{
+		"ogrenciNo":"4",
+		"ogrenciAdi":"Adı",
+		"ogrenciSoyad":"Soyadı",
+		"bolumAdi":"Bilgi İşlem Mühendisliği",
+		"fabrikaAdi":"Elginkan Valfsel",
+		"ogrenciSinif":"1"
+		}
+		]}
+		
